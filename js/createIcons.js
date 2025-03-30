002dcf9ca54fc74422674f3eab62853432883b3c
JXSeaton <j.xseaton@gmail.com>
2025-03-30 16:20:09 -0600

    fixed icon overlap

--- js/createIcons.js
+++ js/createIcons.js
@@ -2,19 +2,36 @@ const icons = document.querySelectorAll("div#skills > div");
 const iconColors = ['#17BEBB', '#F2BB05', '#FF078B'];
 const positions = [];
 
-
-
 function getRandPosition(){
-	const top = Math.floor((Math.random() * 60) + 20) + "%";
-	const left = Math.floor((Math.random() * 40) + 50) + "%";
+	const left = Math.floor((Math.random() * (screen.width - screen.width * 0.5)) + (screen.width * 0.4));
+	const top = Math.floor((Math.random() * (screen.height - screen.height * 0.3)) + (screen.height * 0.1));
 	
-	return [left, top];
+	let notOverlap = true;
+	for(let i = 0; i < positions.length; i++){
+		if(
+			((left >= positions[i][0] && left <= positions[i][0] + 60) ||
+			(left + 60 >= positions[i][0] && left + 60 <= positions[i][0] + 60))
+			&&
+			((top >= positions[i][1] && top <= positions[i][1] + 60) ||
+			(top + 60 >= positions[i][1] && top + 60 <= positions[i][1] + 60))
+		){
+			notOverlap = false;
+		}
+	}
+	if(notOverlap){
+		return [left, top];
+	}else{
+		return false;
+	}
 }
 
 for(let i = 0; i < icons.length; i++){
 	icons[i].style.backgroundColor = iconColors[i % 3];
-	const randPosition = getRandPosition();
+	let randPosition = getRandPosition();
+	while(!randPosition){
+		randPosition = getRandPosition();
+	}
 	positions.push(randPosition);
-	icons[i].style.left = randPosition[0];
-	icons[i].style.top = randPosition[1];
+	icons[i].style.left = randPosition[0] + "px";
+	icons[i].style.top = randPosition[1] + "px";
 }
